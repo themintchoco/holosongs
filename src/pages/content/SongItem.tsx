d1a4ff352eccfd066144cf3c99a7be917c30b914
mint <76428902+themintchoco@users.noreply.github.com>
2022-11-08 00:04:01 +0800

    Adopt new design language (#7)

--- src/pages/content/SongItem.tsx
+++ src/pages/content/SongItem.tsx
@@ -1,14 +1,17 @@
+import cx from 'classnames'
+
 import styles from './SongItem.module.scss'
 import type { Song } from './Content'
 
 export interface SongItemProps {
   song: Song,
+  active?: boolean,
   onSelectSong?: (song: Song) => void,
 }
 
-const SongItem = ({ song, onSelectSong } : SongItemProps) => {
+const SongItem = ({ song, active, onSelectSong } : SongItemProps) => {
   return (
-    <div className={styles.songItem} onClick={() => onSelectSong?.(song)}>
+    <div className={cx(styles.songItem, { [styles.active]: active })} onClick={() => onSelectSong?.(song)}>
       <div className={styles.art} style={{background: 'var(--yt-spec-10-percent-layer)' + (song.art ? ` center/cover url('${song.art}')` : '')}}></div>
       <div className={styles.details}>
         <h3>{song.name}</h3>
