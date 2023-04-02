f08072ab734c7ee368e2fc4904c19e42f57f42fe
mint <76428902+themintchoco@users.noreply.github.com>
2023-04-03 01:12:30 +0800

    Implement update mechanism (#16)

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -2,9 +2,29 @@ import { BrowserMessageType } from '../../common/types/BrowserMessage'
 import { messageOne, messageAll } from '../../common/utils/message'
 import { updateWhitelist } from '../../common/utils/channel-whitelist'
 
-chrome.runtime.onInstalled.addListener(({ reason }) => {
+chrome.runtime.onInstalled.addListener(async ({ reason, previousVersion }) => {
   if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.runtime.openOptionsPage()
+  } else if (reason === chrome.runtime.OnInstalledReason.UPDATE) {
+    const activeTabs = await chrome.tabs.query({ url: '<all_urls>' })
+
+    if (previousVersion === '1.0.0') {
+      // Release v1.0.0 did not implement an update mechanism, so we'll need to manually remove the old components
+      // Remove YoutubePanel and DexLink via their compiled class names
+      await Promise.allSettled(activeTabs.map((tab) => tab.id !== undefined && chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          document.querySelectorAll('.bJwpw5RO98qA7UY6WyUM, .M_ZGC4L_owuKBVoKSvdp').forEach((el) => el.remove())
+        },
+      })))
+    }
+
+    await Promise.allSettled(activeTabs.map((tab) => tab.id !== undefined && chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content.js'],
+    })))
+
+    await messageAll({ type: BrowserMessageType.updateComplete, previousVersion: previousVersion ?? '' })
   }
 })
 
@@ -12,10 +32,6 @@ chrome.alarms.onAlarm.addListener(({ name }) => {
   if (name === 'whitelist-updater') {
     void updateWhitelist()
   }
-})
-
-chrome.runtime.onUpdateAvailable.addListener(({ version }) => {
-  void messageAll({ type: BrowserMessageType.updateAvailable, version })
 })
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
