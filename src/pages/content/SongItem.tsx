a6b82199b3082cedf0ec728f86d65a96f1c6d71f
mint <76428902+themintchoco@users.noreply.github.com>
2025-10-04 23:08:18 -0700

    feat: Add option for fading played songs

--- src/pages/content/SongItem.tsx
+++ src/pages/content/SongItem.tsx
@@ -1,6 +1,7 @@
 import cx from 'classnames'
 
 import styles from './SongItem.module.scss'
+import useStorage from '../../hooks/useStorage'
 import type { Song } from './Content'
 
 export interface SongItemProps {
@@ -11,10 +12,12 @@ export interface SongItemProps {
 }
 
 const SongItem = ({ song, active, played, onSelectSong } : SongItemProps) => {
+  const [fadePlayedSongs] = useStorage('fadePlayedSongs', false)
+
   return (
     <div className={cx(styles.songItem, { [styles.active]: active })} onClick={() => onSelectSong?.(song)}>
       <div className={cx(styles.art, { [styles.placeholder]: !song.art })} style={{backgroundImage: `url("${song.art ?? chrome.runtime.getURL('assets/icons/placeholder.png')}")`}}></div>
-      <div className={cx(styles.details, { [styles.played]: played && !active })}>
+      <div className={cx(styles.details, { [styles.played]: fadePlayedSongs && played && !active })}>
         <h3>{song.name}</h3>
         <h4 className={styles.secondary}>{song.original_artist}</h4>
       </div>
